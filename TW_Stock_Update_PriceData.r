2c8ca88de34693f6f15832ae7dacfcea230dd455
XiaoGeng <xiaogeng423@gmail.com>
2020-04-18 00:16:53 +0800

    Download Stock Code and Price Data

---
+++ TW_Stock_Update_PriceData.r
@@ -0,0 +1,68 @@
+# ===================================================================================
+# =========================================================================== Library
+# ===================================================================================
+library(quantmod)
+# ===================================================================================
+# ===================================================================== Basic Setting
+# ===================================================================================
+data.path      = 'D:/TW_Stock_PriceData/'; dir.create(data.path)
+# ===================================================================================
+# =================================================================== Stock Code List
+# ===================================================================================
+data.colname.vt = c('Open', 'High', 'Low', 'Close', 'Volume', 'Adjusted')
+Git_path   = 'D:/Git_Project/'
+Project_nm = 'AI_Investment'
+code.vt1 = paste0(read.csv(paste0(Git_path, Project_nm, '/TW_Stock_Code1.csv'), 
+				  stringsAsFactors = F)$Code, '.TW')
+code.vt2 = paste0(read.csv(paste0(Git_path, Project_nm, '/TW_Stock_Code2.csv'), 
+				  stringsAsFactors = F)$Code, '.TWO')
+code.vt  = gsub(' ', '', c(code.vt1, code.vt2))
+# ===================================================================================
+# =================================================== Update Data of Daily Open-Close
+# ===================================================================================
+MaxDate.vt = sapply(code.vt, function(code.c){
+	#browser()
+	code = strsplit(code.c, "[.]")[[1]][1]
+	file.name = paste0('TW_Stock_', code)
+	if(file.exists(paste0(data.path, file.name, '.csv'))){
+		old.data.df = read.csv(paste0(data.path, file.name, '.csv'), stringsAsFactors = F)
+		LastDate = tail(old.data.df$Date, 1)
+		FromDate = as.Date(LastDate)+1
+		stock.data = try(getSymbols(code.c, auto.assign = FALSE, from = FromDate))
+		if(class(stock.data) != 'try-error'){
+			#browser()
+			stock.df = as.data.frame(stock.data)
+			colnames(stock.df) = data.colname.vt
+			stock.df = cbind('Date' = rownames(stock.df), stock.df)
+			rownames(stock.df) = NULL
+			new.data.df = rbind(old.data.df, stock.df)
+			colnames(new.data.df) = c('Date', data.colname.vt)
+		}
+		else{
+			new.data.df = old.data.df
+			colnames(new.data.df) = c('Date', data.colname.vt)
+		}
+		write.csv(new.data.df, paste0(data.path, file.name, '.csv'), row.names = F)
+		as.character(tail(stock.df$Date, 1))
+	}
+	else{
+		stock.data = try(getSymbols(code.c, auto.assign = FALSE, from = '2000-01-01"'))
+		if(class(stock.data) != 'try-error'){
+			#browser()
+			stock.df = as.data.frame(stock.data)
+			colnames(stock.df) = data.colname.vt
+			stock.df = cbind('Date' = rownames(stock.df), stock.df)
+			rownames(stock.df) = NULL
+			write.csv(stock.df, paste0(data.path, file.name, '.csv'), row.names = F)
+			as.character(tail(stock.df$Date, 1))
+		}
+		else{
+			NA
+		}
+	}
+})
+write.csv(cbind('Code' = names(MaxDate.vt), 'LastDate' = MaxDate.vt), 
+          paste0(Git_path, Project_nm, '/TW_Stock_CodeCheck.csv'), row.names = F)
+# ===================================================================================
+# ===================================================================================
+# ===================================================================================
